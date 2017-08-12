39e699a91df9a1f1a0f000d79391ce675d479f35
Kevin Williams <kwilliams@priworks.com>
2017-08-12 09:47:45 -0400

    Setup slices and core page

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	
+	$('.content-slider').slick({
+		dots:true,
+		prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"><span class="arrow"><img src="../assets/images/arrow-left.png" alt=""></span> <span class="arrow-text">Previous</span></button>',
+        nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"><span class="arrow"><img src="../assets/images/arrow-right.png" alt=""></span> <span class="arrow-text">Next</span></button>',	
+	});
+	
+});
